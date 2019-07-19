667a899fba9e3af12b6ebc86d29dfb68b23421f8
Michael Hewner <hewner@gmail.com>
2019-07-19 14:45:19 -0400

    a few notes for classes

--- Sessions/03-Raindrops/StevesObjectExamples/EmployeeClass.py
+++ Sessions/03-Raindrops/StevesObjectExamples/EmployeeClass.py
@@ -1,14 +1,11 @@
 # First we define the class:
 
 class Employee:
-   'Common base class for all employees'
-   empCount = 0
 
    def __init__(self, name, salary):
       self.name = name
       self.salary = salary
-      Employee.empCount += 1
-   
+
    def displayCount(self):
      print ("Total Employee %d" % Employee.empCount)
 
@@ -29,9 +26,14 @@ emp2 = Employee("Manni", 5000)
 
 emp1.displayEmployee()
 emp2.displayEmployee()
-print ("Total Employee %d" % Employee.empCount)
 
 emp1.age = 7  # Add an 'age' attribute.
 emp1.age = 8  # Modify 'age' attribute.
 print ("Employee 1 age is ",emp1.age)
-del emp1.age  # Delete 'age' attribute.
+
+# same kind of thing but usings lists
+# lets give everbody a raise!
+all_employees = [emp1, emp2]
+for e in all_employees:
+   e.salary = e.salary + 100
+   e.displayEmployee()
